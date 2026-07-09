7af5f4c454f3c090e2c4e75a7f4d95c9812a3cb3
spyderx3 <anugusuhaas@gmail.com>
2026-07-09 10:20:55 -0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Paste the URL of your backend here once it's deployed on Render.
-// Example: "https://pose-backend-xyz.onrender.com"
-// No trailing slash at the end.
-const BACKEND_URL = "https://passionprojbackend-1.onrender.com";
